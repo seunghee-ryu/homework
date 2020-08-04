a71beb45bcff0c8b0b17b78139500a3f66f80f12
seunghee-ryu <shee.ryu88@gmail.com>
2020-08-04 20:32:27 +0900

    p

--- src/main/java/com/tarae/practice/App.java
+++ src/main/java/com/tarae/practice/App.java
@@ -5,14 +5,6 @@ import java.util.Scanner;
 
 public class App {
 
-  static long currentMillis;
-  static Scanner sc = new Scanner(System.in);
-  static int count = 0;
-  static final int LENGTH = 100;
-  static int i = 0;
-  static Member[] members = new Member[LENGTH];
-
-
   public static void main(String[] args) {
 
     class Member {
@@ -25,66 +17,61 @@ public class App {
       Date regiDate;
     }
 
+    Scanner sc = new Scanner(System.in);
+    int count = 0;
+    final int LENGTH = 100;
+    int i = 0;
+    Member[] members = new Member[LENGTH];
+
+
     System.out.println("[회원 등록]");
-    addMembers();
-    System.out.println("[회원 목록]");
-    listMembers();
-
-
-  }
-
-
-  static void addMembers() {
-
-    System.out.println("[회원]");
 
     for (i = 0; i < LENGTH; i++) {
       count++;
 
-    Member m = new Member();
+      Member m = new Member();
 
 
-    System.out.print("번호? ");
-    m.no = sc.nextInt();
-    sc.nextLine();
+      System.out.print("번호? ");
+      m.no = sc.nextInt();
+      sc.nextLine();
 
-    System.out.print("이름?");
-    m.name = sc.nextLine();
+      System.out.print("이름?");
+      m.name = sc.nextLine();
 
-    System.out.print("이메일? ");
-    m.email = sc.nextLine();
+      System.out.print("이메일? ");
+      m.email = sc.nextLine();
 
-    System.out.print("패스워드? ");
-    m.password = sc.nextLine();
+      System.out.print("패스워드? ");
+      m.password = sc.nextLine();
 
-    System.out.print("사진? ");
-    m.photo = sc.nextLine();
+      System.out.print("사진? ");
+      m.photo = sc.nextLine();
 
-    System.out.print("전화? ");
-    m.tel = sc.nextLine();
+      System.out.print("전화? ");
+      m.tel = sc.nextLine();
 
-    System.out.println("등록일? ");
-    m.regiDate = new Date(currentMillis);
+      System.out.println("등록일? ");
+      m.regiDate = Date.valueOf(sc.nextLine());
 
+      members[i] = m;
 
-    System.out.print("계속 입력하시겠습니까?(y/N) ");
-    String response = sc.nextLine();
+      System.out.print("계속 입력하시겠습니까?(y/N) ");
+      String response = sc.nextLine();
 
-    if (!response.equalsIgnoreCase("y")) {
-      break;
+      if (!response.equalsIgnoreCase("y")) {
+        break;
+      }
+
+      sc.close();
     }
 
-    members[i] = m;
-    sc.close();
-  }
-
-}
-
-  static void listMembers() {
-    for (int i = 0; i < count; i++) {
+    for (i = 0; i < count; i++) {
       Member m = members[i];
       System.out.printf("%d, %s, %s, %s, %s\n",
-          m.no, m.name, m.email, m.tel, m.regiDate.toString());
+          m.no, m.name, m.email, m.tel, m.regiDate);
     }
+
+
   }
 }
